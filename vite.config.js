1c0d6ca4735b0101af46da00dc602555ae410bf6
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-02-12 17:25:51 +0100

    add svgr plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/event-table/',
-  plugins: [react()],
+  plugins: [react(), svgr()],
   build: {
     outDir: 'dist',
-     assetsDir: 'assets'
+    assetsDir: 'assets',
   },
 })
