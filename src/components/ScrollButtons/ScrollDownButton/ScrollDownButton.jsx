f850ad2da602b6e63d0e7ff6bf8a29bb23c85a59
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-03-20 16:38:31 +0100

    restructure button components

--- src/components/ScrollButtons/ScrollDownButton/ScrollDownButton.jsx
+++ src/components/ScrollButtons/ScrollDownButton/ScrollDownButton.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import ArrowDown from '../../assets/icons/down-arrow.svg?react'
+import ArrowDown from '../../../assets/icons/down-arrow.svg?react'
 import './ScrollDownButton.scss'
 
 const ScrollDownButton = () => {
