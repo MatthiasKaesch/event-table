9d66862510d32acf83aaa75cb0809857920acdab
MatthiasKaesch <kaesch.web.development@gmail.com>
2025-05-02 12:08:11 +0200

    add room icon

--- src/components/Roomlist/Roomlist.jsx
+++ src/components/Roomlist/Roomlist.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import DoorIcon from '../../assets/icons/door.svg?react'
 import './Roomlist.scss'
 
 const Roomlist = ({ rooms }) => {
   return (
     <ul role="list" className="rooms_list">
-      {rooms.map((room) => {
+      {rooms.map((room, index) => {
         return (
           <li className="rooms_list__item" key={room.name}>
-            {room.name}
+            {index !== 0 && <DoorIcon />}
+            <p>{room.name}</p>
           </li>
         )
       })}
